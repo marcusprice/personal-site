2eeddfa3dd0cd697f8ac2769895917ecc26f7eb5
marcusprice <marcusprice88@gmail.com>
2022-07-24 23:58:34 -0700

    adds express web server

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express()
+const port: number = 5000
+
+app.use(express.static("./public"))
+
+app.get("/webook", (_, res) => {
+    console.log("run git pull command here")
+    res.status(200).send("suh")
+})
+
+app.listen(port, () => {
+    console.log("listening on port", port)
+})
