2644c9e67b2ad048511a554c9b5efceb7f761137
marcusprice <marcusprice88@gmail.com>
2022-07-25 00:04:35 -0700

    adds express types

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import express from 'express'
-const app = express()
+import express, { Express, Response } from 'express'
+const app: Express = express()
 const port: number = 5000
 
 app.use(express.static("./public"))
 
-app.get("/webook", (_, res) => {
+app.get("/webook", (_, res: Response) => {
     console.log("run git pull command here")
     res.status(200).send("suh")
 })
